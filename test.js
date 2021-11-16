cbac7e29ea99a1cda6a10f4879a748d0a877be41
jehdz <33530937+jehdz@users.noreply.github.com>
2021-11-15 18:41:37 -0600

    Add files via upload

---
+++ test.js
@@ -0,0 +1,5 @@
+url3 = $('#url3').val();
+imgUrl3 = $('#imageUrl3').val();
+product3 = $('#product3').val();
+productSalePrice3 = $('#productSalePrice3').val();
+productRegPrice3 = $('#productRegPrice3').val();
