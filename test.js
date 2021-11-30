a1211a525758241075cf3939bf68fbfaff7eacb3
jehdz <33530937+jehdz@users.noreply.github.com>
2021-11-29 19:43:15 -0600

    Delete test.js

--- test.js
+++
@@ -1,5 +0,0 @@
-url3 = $('#url3').val();
-imgUrl3 = $('#imageUrl3').val();
-product3 = $('#product3').val();
-productSalePrice3 = $('#productSalePrice3').val();
-productRegPrice3 = $('#productRegPrice3').val();
